4046cdc604cc78d5b3604d41d93903baa5424fe2
Unknown <vl979@nyu.edu>
2017-03-07 12:09:06 -0500

    Whoops

---
+++ Assets/Attack.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	bool hitEnemy = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		
+
+	}
+
+
+	void OnCollisionEnter2D (Collision2D gameObjectHittingme)
+	{
+
+		if (hitEnemy == false && (Input.GetKey (KeyCode.Space))) {
+			//GetComponent<Rigidbody2D>().
+			hitEnemy = true;
+		} else {
+			hitEnemy = false;
+		}
+
+		if (hitEnemy == true && (gameObjectHittingme.gameObject.tag == "Enemy")){
+			//			Debug.Log("HI");
+			//			GetComponent<Rigidbody2D>().AddForce(transform.up*3,ForceMode2D.Impulse);
+			Debug.Log ("hitting enemy");
+			Destroy (gameObjectHittingme.gameObject);
+		}
+			}
+
+	}
+
+
---
+++ Assets/TestPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour {
+
+	public float moveSpeed = 5f;
+	public float timeCharged = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPos = transform.position;
+		Vector3 currentScale = transform.localScale;
+
+		if (Input.GetKey (KeyCode.D)) {
+			currentPos.x += moveSpeed * Time.deltaTime;
+		}
+		else if (Input.GetKey (KeyCode.A)) {
+			currentPos.x -= moveSpeed * Time.deltaTime;
+		}
+
+		//if (Input.GetKeyDown (KeyCode.LeftShift)) {
+		//	moveSpeed = moveSpeed / Time.deltaTime;
+		//} else if (Input.GetKeyUp (KeyCode.LeftShift)) {
+			//moveSpeed = 5f;
+		//}
+
+
+		transform.localScale = currentScale;
+		transform.position = currentPos;
+		
+	}
+}
